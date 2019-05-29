901ae9cd3ab0d811ba5fbae10c1763b4db7981f3
15744 <1574408801@qq.com>
2019-05-29 10:56:52 +0800

    commit

--- src/components/productionMonitoring/workStationInspectionRecord.vue
+++ src/components/productionMonitoring/workStationInspectionRecord.vue
@@ -9,15 +9,16 @@
         <div class="template-content">
             <div class="container">
                 <div class="handle-box">
-                    <label style="margin-right: 10px">
-                        <span>智能检索托单金物</span>
+                    <label style="margin-right: 5px">
+                        <span>检索点检记录</span>
                         <span>:</span>
-                        <el-input v-model="select_word" placeholder="智能检索托单金物" class="handle-input mr10"></el-input>
+                        <el-input v-model="select_word" placeholder="检索点检记录" class="handle-input mr10" style="width: 150px"></el-input>
                     </label>
-                    <label style="margin-right: 10px;margin-left: 10px">
-                        <span>选择查询时间</span>
+                    <label style="margin-right: 5px;margin-left: 5px">
+                        <span>时间</span>
                         <span>:</span>
                         <el-date-picker
+                            style="width: 240px"
                             v-model="examineTime"
                             type="daterange"
                             start-placeholder="开始日期"
@@ -25,7 +26,47 @@
                             value-format="yyyy-MM-dd">
                         </el-date-picker>
                     </label>
-                    <el-button type="primary" icon="delete" class="handle-del mr10" @click="doSearch">查询报表</el-button>
+                    <label style="margin-right: 5px;margin-left: 5px">
+                        <span>生产线</span>
+                        <span>:</span>
+                        <el-select
+                            style="width: 120px"
+                            v-model="line"
+                            clearable
+                            filterable
+                            allow-create
+                            default-first-option
+                            @change="changeSCX"
+                            placeholder="请选择生产线">
+                            <el-option
+                                v-for="item in lineOptions"
+                                :key="item.indexno"
+                                :label="item.name"
+                                :value="item.indexno">
+                            </el-option>
+                        </el-select>
+                    </label>
+                    <label style="margin-right: 10px;margin-left:5px">
+                        <span>工位</span>
+                        <span>:</span>
+                        <el-select
+                            style="width: 120px"
+                            v-model="workStation"
+                            clearable
+                            filterable
+                            allow-create
+                            default-first-option
+                            @change="changeSelect"
+                            placeholder="请选择工位">
+                            <el-option
+                                v-for="item in workStationOptions"
+                                :key="item.id"
+                                :label="item.name"
+                                :value="item.id">
+                            </el-option>
+                        </el-select>
+                    </label>
+                    <el-button type="primary" icon="delete" class="handle-del mr10" @click="showAdd">进行点检</el-button>
                 </div>
                 <div class="">
                     <el-table class="tb-edit"
@@ -33,14 +74,65 @@
                               :header-cell-style="{background:'#A1D0FC',color:'rgba(0, 0, 0, 0.8)',fontSize:'20px'}"
                               border
                               height="450"
+                              @select-all="selectAll"
+                              @select="selectList"
+                              @row-dblclick="edit"
                               highlight-current-row
                               style="width: 98%;margin: auto">
+                        <el-table-column
+                            type="selection"
+                            width="30">
+                        </el-table-column>
                         <template v-for="(col ,index) in cols">
                             <el-table-column align="center" :prop="col.prop" :label="col.label"></el-table-column>
                         </template>
                     </el-table>
                 </div>
             </div>
+            <!--新增弹出框 -->
+            <el-dialog title="进行点检" :visible.sync="addVisible" width="40%">
+                <el-form ref="form"  label-width="100px">
+                    <el-form-item label="记录内容">
+                        <el-input v-model="jvnr" style="width: 200px"></el-input>
+                    </el-form-item>
+                    <el-form-item label="点检状态">
+                        <el-input v-model="djzt" style="width: 200px"></el-input>
+                    </el-form-item>
+                    <el-form-item label="备注">
+                        <el-input v-model="beizhu" style="width: 200px"></el-input>
+                    </el-form-item>
+                </el-form>
+                <span slot="footer" class="dialog-footer">
+                <el-button @click="addVisible = false" style="height:30px;width:80px">取 消</el-button>
+                <el-button type="primary" @click="doAdd" style="height:30px;width:80px">确 定</el-button>
+            </span>
+            </el-dialog>
+            <!-- 编辑弹出框 -->
+            <el-dialog title="修改点击记录" :visible.sync="editVisible" width="40%">
+                <el-form ref="form"  label-width="100px">
+                    <el-form-item label="记录内容">
+                        <el-input v-model="jvnr" style="width: 200px"></el-input>
+                    </el-form-item>
+                    <el-form-item label="点检状态">
+                        <el-input v-model="djzt" style="width: 200px"></el-input>
+                    </el-form-item>
+                    <el-form-item label="备注">
+                        <el-input v-model="beizhu" style="width: 200px"></el-input>
+                    </el-form-item>
+                </el-form>
+                <span slot="footer" class="dialog-footer">
+                <el-button @click="editVisible = false" style="height:30px;width:80px">取 消</el-button>
+                <el-button type="primary" @click="saveEdit" style="height:30px;width:80px">确 定</el-button>
+            </span>
+            </el-dialog>
+            <!-- 删除提示框 -->
+            <el-dialog title="删除点检记录" :visible.sync="delVisible" width="300px" center>
+                <div class="del-dialog-cnt">删除不可恢复，是否确定删除？</div>
+                <span slot="footer" class="dialog-footer">
+                <el-button @click="delVisible = false" style="height:30px;width:80px">取 消</el-button>
+                <el-button type="primary" @click="deleteRow" style="height:30px;width:80px">确 定</el-button>
+            </span>
+            </el-dialog>
 
             <Modal :msg="message"
                    :isHideModal="HideModal"></Modal>
@@ -51,7 +143,6 @@
     import axios from 'axios'
     import url from '../../assets/js/URL'
     import Modal from '../../common/modal'
-    import {getNowTime} from '../../assets/js/api'
 
     export default {
         name: 'WorkingProcedure',
@@ -59,17 +150,27 @@
             return {
                 message: '',
                 HideModal: true,
+                listData:[],
+                id:"",
+
 
                 cols: [],
                 tableData: [],
 
-                batch: "",
-                batchOptions: [],
-
                 select_word: '',
-                examineTime: "",
+
+                addVisible: false,
+                editVisible: false,
 
 
+                workStation:"",
+                workStationOptions:[],
+                line: '',
+                lineOptions: [],
+
+                jvnr:"",
+                djzt:"",
+                beizhu:"",
             }
         },
         computed: {
@@ -103,21 +204,17 @@
                     this.$router.push("/")
                 }
                 else {
-                    let time = getNowTime();
-                    let times = [];
-                    for (let i = 0; i < 2; i++) {
-                        times.push(time)
-                    }
-                    this.examineTime = times;
-
                     let that = this;
                     axios.all([
-                        axios.post(" " + url + "/sys/getPiciList"),
+                        axios.post(" " + url + "/sys/dictionaryList", {"id": "9"}),
+                        axios.post(" " + url + "/api/getPersonProcessList", {"name": ""}),
                     ])
-                        .then(axios.spread(function (select) {
-                            that.batchOptions = select.data;
-                            that.batch = select.data[0].id;
-                            that.loadingShowData(that.batch);
+                        .then(axios.spread(function (line,workStation) {
+                            that.lineOptions = line.data;
+                            that.line = line.data[0].indexno;
+                            that.workStation = workStation.data[0].id;
+                            that.workStationOptions = workStation.data;
+                            that.loadingShowData(1);
                         }));
                 }
             },
@@ -126,33 +223,164 @@
             loadingShowData(data) {
                 let that = this;
                 axios.all([
-                    axios.post(" " + url + "/sys/showTableTitle", {"name": "zwjwcx"}),
-                    axios.post(" " + url + "/wuliao/jinwuZhuwenpinList", {"time": data})
+                    axios.post(" " + url + "/sys/showTableTitle", {"name": "gwdjjv"}),
+                    axios.post(" " + url + "/padShow/buttonList", {"id": data})
                 ])
                     .then(axios.spread(function (title, table) {
                         that.cols = title.data;
-                        that.tableData = table.data;
+                        that.tableData = table.data.data;
                     }));
             },
 
+            changeSCX(){
+                axios.post(" " + url + "/sysconfig/getGongxuList", {"id": this.line})
+                    .then((res) => {
+                        this.workStation = res.data[0].id;
+                        this.workStationOptions = res.data;
+                        this.loadingShowData(this.workStation)
+                    });
+            },
 
-            //根据时间查询
-            doSearch() {
-                if (this.batch) {
-                    this.loadingShowData(this.batch)
+            //根据工位选择
+            changeSelect() {
+                this.loadingShowData(this.workStation)
+            },
+
+            //选择那个一个
+            selectList(val) {
+                if (val.length) {
+                    let data = [];
+                    for (let i = 0; i < val.length; i++) {
+                        let a = val[i].id;
+                        data.push(a)
+                    }
+                    this.listData = data;
                 }
                 else {
-                    this.message = "查询批次不能为空";
+                    this.listData=[];
+                }
+            },
+
+            //列表全部选择
+            selectAll(val) {
+                if (val.length) {
+                    let data = [];
+                    for (let i = 0; i < val.length; i++) {
+                        let a = val[i].id;
+                        data.push(a)
+                    }
+                    this.listData = data;
+                }
+                else {
+                    this.listData = [];
+                }
+            },
+
+            //显示新增
+            showAdd(){
+
+                if (this.listData.length) {
+                    this.addVisible = true;
+                }
+                else {
+                    this.message = "请勾选要点检的工位";
                     this.HideModal = false;
                     const that = this;
+
                     function a() {
                         that.message = "";
                         that.HideModal = true;
                     }
+
                     setTimeout(a, 2000);
                 }
+            },
 
-            }
+            //进行新增
+            doAdd() {
+                if (this.name && this.type && this.disabled &&this.backgroundColor&&this.showHide) {
+                    axios.post(" " + url + "/padShow/buttonAdd",
+                        {
+                            "gongxuid": this.workStation,
+                            "name": this.name,
+                            "type": this.type,
+                            "disabled": this.disabled,
+                            "backgroundcolor": this.backgroundColor,
+                            "show": this.showHide,
+                        }
+                    )
+                        .then((res) => {
+                            if (res.data.state === "1") {
+                                this.$message.success(`新增成功`);
+                                this.addVisible = false;
+                                this.loadingShowData(this.workStation)
+
+                            }
+                            else {
+                                this.$message.warning(`新增失败`);
+                            }
+                        })
+                        .catch((err) => {
+                            console.log(err)
+                        })
+                }
+                else {
+                    this.$message.warning(`输入不能为空`);
+                }
+            },
+
+            //双击点击行内编辑
+            edit(row, column, cell, event) {
+                this.editVisible = true;
+                this.id = row.id;
+                axios.post(" " + url + "/padShow/buttonDetail", {"id": this.id})
+                    .then((res) => {
+                        this.workStation = res.data.data.gongxuid;
+                        this.name = res.data.data.name;
+                        this.type = Number(res.data.data.type);
+                        this.disabled = res.data.data.disabled;
+                        this.backgroundColor = res.data.data.backgroundcolor;
+                        this.showHide = res.data.data.show;
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    });
+            },
+
+            // 保存编辑
+            saveEdit() {
+                if (this.name && this.type && this.disabled &&this.backgroundColor&&this.showHide) {
+                    axios.post(" " + url + "/padShow/buttonUpdate",
+                        {
+                            "id":this.id,
+                            "gongweiid": this.workStation,
+                            "name": this.name,
+                            "type": this.type,
+                            "disabled": this.disabled,
+                            "backgroundcolor": this.backgroundColor,
+                            "show": this.showHide,
+                        }
+                    )
+                        .then((res) => {
+                            if (res.data.state === "1") {
+                                this.editVisible = false;
+                                this.$message.success(`修改成功`);
+                                this.loadingShowData(this.workStation)
+                            }
+                            else {
+                                this.$message.warning(`新增失败`);
+                            }
+                        })
+                        .catch((err) => {
+                            console.log(err)
+                        })
+                }
+                else {
+                    this.$message.warning(`输入不能为空`);
+                }
+
+            },
+
 
         }
     }
@@ -172,14 +400,14 @@
         .template-content {
             .handle-box {
                 height: 80px;
-                line-height: 80px;
+                line-height:80px;
                 padding-left: 50px;
                 .handle-input {
                     width: 300px;
                     display: inline-block;
                 }
                 .el-button {
-                    width: 100px;
+                    width:100px;
                     height: 30px;
                 }
             }
