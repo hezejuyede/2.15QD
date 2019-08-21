6b17c2f501d09bac533a4e3fa3d276314d145ff9
15744 <1574408801@qq.com>
2019-08-21 09:24:19 +0800

    commit

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="template">
        <!-- <iframe src="http://157.116.13.17:8080/WebReport/ReportServer?formlet=dapingdemo.frm" style="width: 100%;height: 100%"></iframe>-->
-        <iframe src="http://www.nacks.com.cn" style="width: 100%;height: 100%"></iframe>
+     <!--   <iframe src="http://www.nacks.com.cn" style="width: 100%;height: 100%"></iframe>-->
+        <div class=""><img src="../../assets/img/Winlogon-bg.png" alt="" style="width: 100%;height: 100%"></div>
        <!-- <div id="banner">
             <swiper :options="swiperOption" ref="mySwiper">
                 <swiper-slide v-for="(item,index) in bannerImg" :key="item.id">
