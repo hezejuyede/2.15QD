4e8689f8964015de89e0ad7a13247c3af87e0743
15744 <1574408801@qq.com>
2019-08-23 15:10:22 +0800

    commit

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,25 +1,13 @@
 <template>
     <div class="template">
-       <!-- <iframe src="http://157.116.13.17:8080/WebReport/ReportServer?formlet=dapingdemo.frm" style="width: 100%;height: 100%"></iframe>-->
-     <!--   <iframe src="http://www.nacks.com.cn" style="width: 100%;height: 100%"></iframe>-->
-        <div class=""><img src="../../assets/img/Winlogon-bg.png" alt="" style="width: 100%;height: 100%"></div>
-       <!-- <div id="banner">
-            <swiper :options="swiperOption" ref="mySwiper">
-                <swiper-slide v-for="(item,index) in bannerImg" :key="item.id">
-                    <img :src="item.img"
-                         style="width: 100%">
-                </swiper-slide>
-                <div class="swiper-pagination" slot="pagination"></div>
-                <div class="swiper-scrollbar" slot="scrollbar"></div>
-            </swiper>
-        </div>-->
+
     </div>
 </template>
 <script type="text/ecmascript-6">
     import axios from 'axios'
     import url from '../../assets/js/URL'
-    import Modal from '../../common/modal'
     import {swiper, swiperSlide} from 'vue-awesome-swiper';
+
     require('swiper/dist/css/swiper.css');
 
     export default {
@@ -44,21 +32,8 @@
                 }
             }
         },
-        computed: {
-            //模糊检索
-            tables: function () {
-                var search = this.select_word;
-                if (search) {
-                    return this.tableData.filter(function (dataNews) {
-                        return Object.keys(dataNews).some(function (key) {
-                            return String(dataNews[key]).indexOf(search) > -1
-                        })
-                    })
-                }
-                return this.tableData
-            }
-        },
-        components: {Modal,swiper, swiperSlide,},
+        computed: {},
+        components: {swiper, swiperSlide,},
         mounted() {
 
 
@@ -67,7 +42,6 @@
             this.getAdminState();
         },
         methods: {
-
             //页面加载检查用户是否登陆，没有登陆就加载登陆页面
             getAdminState() {
                 const userInfo = localStorage.getItem("userInfo");
@@ -75,17 +49,7 @@
                     this.$router.push("/")
                 }
                 else {
-                    let that = this;
-                    axios.all([
-                        axios.post(" " + url + "/api/getPersonProcessList", {"name": ""}),
-                        axios.post(" " + url + "/sys/dictionaryList", {"id": "23"}),
-                    ])
-                        .then(axios.spread(function (select, type) {
-                            that.workStation = select.data[0].id;
-                            that.workStationOptions = select.data;
-                            that.typeOptions = type.data;
-                            that.loadingShowData(1);
-                        }));
+
                 }
             },
 
@@ -98,7 +62,8 @@
     .template {
         width: 100%;
         height: 100%;
-        background-color: @color-white;
+        background-image: url("../../assets/img/Winlogon-bg.png");
+        background-size: cover;
 
     }
 
