0e3488c8f5daf1b290c2adaee903d0110ebe86db
15744 <1574408801@qq.com>
2019-09-02 15:08:23 +0800

    commit

--- src/components/materialManagement/cPReportForm.vue
+++ src/components/materialManagement/cPReportForm.vue
@@ -160,14 +160,13 @@
 
 
             //导出打印
-            importPrinting(){
+            importPrinting() {
                 let wb = XLSX.utils.table_to_book(document.querySelector('#rebateSetTable'));
                 /* get binary string as output */
-                let wbout = XLSX.write(wb, { bookType: 'xlsx', bookSST: true, type: 'array' });
+                let wbout = XLSX.write(wb, {bookType: 'xlsx', bookSST: true, type: 'array'});
                 try {
-                    FileSaver.saveAs(new Blob([wbout], { type: 'application/octet-stream' }), '储品金物报表.xlsx');
-                } catch (e)
-                {
+                    FileSaver.saveAs(new Blob([wbout], {type: 'application/octet-stream'}), '储品金物报表.xlsx');
+                } catch (e) {
                     if (typeof console !== 'undefined')
                         console.log(e, wbout)
                 }
