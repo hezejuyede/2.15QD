5c21fd0b0c4b246f58227bd1cdcc2995c4699a2c
15744 <1574408801@qq.com>
2019-09-27 11:02:20 +0800

    commit

--- src/components/materialManagement/tdjwDelivery.vue
+++ src/components/materialManagement/tdjwDelivery.vue
@@ -51,6 +51,7 @@
                         </el-select>
                     </label>
                     <el-button type="primary"  @click="doSearch">查询</el-button>
+                    <el-button type="success"  @click="showDoSee">查看一品图</el-button>
                 </div>
                 <div class="">
                     <el-table class="tb-edit"
@@ -58,14 +59,35 @@
                               :header-cell-style="{background:'#A1D0FC',color:'rgba(0, 0, 0, 0.8)',fontSize:'20px'}"
                               border
                               :height="this.tableHeight"
+                              @select="selectList"
+                              @select-all="selectAll"
+                              @selection-change="selectChange"
                               highlight-current-row
                               style="width: 98%;margin: auto">
+                        <el-table-column
+                            type="selection"
+                            width="30">
+                        </el-table-column>
                         <template v-for="(col ,index) in cols">
                             <el-table-column align="center" :prop="col.prop" :label="col.label"></el-table-column>
                         </template>
                     </el-table>
                 </div>
             </div>
+
+
+            <!--新增弹出框 -->
+            <el-dialog title="新增按钮" :visible.sync="yptVisible" :fullscreen="true" :center="true">
+               <div class="" style="height: 800px;overflow: auto" ref="imageWrapper">
+                   <viewer :images="imgs">
+                       <img v-for="src in imgs" :src="src.url" :key="src.title" style="width: 100%;height: 100%">
+                   </viewer>
+               </div>
+                <span slot="footer" class="dialog-footer">
+                <el-button @click="yptVisible = false" style="height:30px;width:80px">取 消</el-button>
+             <!--   <el-button type="primary" @click="importImg" style="height:30px;width:80px">打印一品图</el-button>-->
+            </span>
+            </el-dialog>
 
             <Modal :msg="message"
                    :isHideModal="HideModal"></Modal>
@@ -76,7 +98,7 @@
     import axios from 'axios'
     import url from '../../assets/js/URL'
     import Modal from '../../common/modal'
-    import {getNowTime} from '../../assets/js/api'
+    import html2canvas from 'html2canvas'
 
     export default {
         name: 'WorkingProcedure',
@@ -88,6 +110,9 @@
                 cols: [],
                 tableData: [],
 
+                listData:[],
+                id:"",
+
                 batch: "",
                 batchOptions: [],
 
@@ -96,6 +121,10 @@
                 tableHeight:Number,
                 peisong: "",
                 peisongOptions: [{"name": "已送货", "id": "1"}, {"name": "已配材", "id": "2"}, {"name": "未配材", "id": "3"}, {"name": "总清单", "id": "4"}],
+                yptVisible:false,
+                url: "",
+                imgs: [],
+
 
 
             }
@@ -169,6 +198,50 @@
                     }));
             },
 
+            //选择那个一个
+            selectList(val) {
+                if (val.length) {
+                    let data = [];
+                    for (let i = 0; i < val.length; i++) {
+                        let a = val[i].id;
+                        data.push(a)
+                    }
+                    this.listData = data;
+                }
+                else {
+                    this.listData=[];
+                }
+            },
+
+            //列表全部选择
+            selectAll(val) {
+                if (val.length) {
+                    let data = [];
+                    for (let i = 0; i < val.length; i++) {
+                        let a = val[i].id;
+                        data.push(a)
+                    }
+                    this.listData = data;
+                }
+                else {
+                    this.listData = [];
+                }
+            },
+
+            //选择改变
+            selectChange(val){
+                if (val.length) {
+                    let data = [];
+                    for (let i = 0; i < val.length; i++) {
+                        let a = val[i].id;
+                        data.push(a)
+                    }
+                    this.listData = data;
+                }
+                else {
+                    this.listData=[];
+                }
+            },
 
             //根据时间查询
             doSearch() {
@@ -186,6 +259,79 @@
                     setTimeout(a, 2000);
                 }
 
+            },
+
+            //显示查询一品图
+            showDoSee(){
+                if (this.listData.length) {
+                    if(this.listData.length>1){
+                        this.$message.warning("只能查看一个");
+                    }
+                    else {
+                        let yiguanhao;
+                        let code;
+                        for (let i = 0; i < this.tableData.length; i++) {
+                            if (this.tableData[i].id === this.listData[0]) {
+                                yiguanhao = this.tableData[i].yiguanhao;
+                                code = this.tableData[i].codeno;
+                            }
+                        }
+
+                        axios.post(" " + url + "/yipintu/getYipintuImg.html", {
+                            "pici": this.batch,
+                            "yiguanhao": yiguanhao,
+                            "code": code
+                        })
+                            .then((res) => {
+                                if (res.data.imgurl) {
+                                    this.yptVisible = true;
+                                    this.url = url + res.data.imgurl;
+                                    this.imgs = [{"url": this.url}];
+                                }
+                                else {
+                                    this.message = "没有查到一品图";
+                                    this.HideModal = false;
+                                    const that = this;
+
+                                    function a() {
+                                        that.message = "";
+                                        that.HideModal = true;
+                                    }
+
+                                    setTimeout(a, 2000);
+                                }
+                            })
+                            .catch((err) => {
+                                console.log(err)
+                            })
+                    }
+                }
+                else {
+                    this.message = "请勾选";
+                    this.HideModal = false;
+                    const that = this;
+
+                    function a() {
+                        that.message = "";
+                        that.HideModal = true;
+                    }
+
+                    setTimeout(a, 2000);
+                }
+            },
+
+            //导出一品图
+            importImg() {
+                html2canvas(this.$refs.imageWrapper, {
+                    backgroundColor: null     // 解决生成的图片有白边
+                }).then((canvas) => {
+                    let dataURL = canvas.toDataURL("image/png");
+                    let a = document.createElement('a');
+                    a.setAttribute('download', '');
+                    a.setAttribute('href', dataURL);
+                    a.setAttribute("target", "_blank");
+                    a.click();
+                })
             }
 
         }
